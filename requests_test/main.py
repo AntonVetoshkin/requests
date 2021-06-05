f7f7062cd6fd39012b8a62bbe91df3a9d55264f7
AntonVetoshkin <aveto77@mail.ru>
2021-06-05 23:18:10 +0500

    task 2 fixed

--- requests_test/main.py
+++ requests_test/main.py
@@ -1,21 +1,21 @@
 from pprint import pprint
-
-import requests
+# import requests
 # TASK 1
-heroes_list = ['Hulk', 'Captain America', 'Thanos']
-for heroes_name in heroes_list:
-    response = requests.get(f'https://superheroapi.com/api/2619421814940190/search/{heroes_name}')
-    heroes_intelligence = int(response.json()['results'][0]['powerstats']['intelligence'])
-    best_intelligence = 0
-    clever_hero = ''
-    if heroes_intelligence > best_intelligence:
-        best_intelligence = heroes_intelligence
-        clever_hero = heroes_name
-print(clever_hero, best_intelligence)
+# heroes_list = ['Hulk', 'Captain America', 'Thanos']
+# for heroes_name in heroes_list:
+#     response = requests.get(f'https://superheroapi.com/api/2619421814940190/search/{heroes_name}')
+#     heroes_intelligence = int(response.json()['results'][0]['powerstats']['intelligence'])
+#     best_intelligence = 0
+#     clever_hero = ''
+#     if heroes_intelligence > best_intelligence:
+#         best_intelligence = heroes_intelligence
+#         clever_hero = heroes_name
+# print(clever_hero, best_intelligence)
 
 # def test_request():
 #     url = "https://cloud-api.yandex.net/v1/disk/resources/files"
-#     headers = {'Authorization': 'OAuth AQAAAAAFb6IVAADLW6pIWXkR-k0gsGDbokios1I', 'Content-Type': 'application/json'}
+#     TOKEN =
+#     headers = {'Authorization': 'OAuth TOKEN, 'Content-Type': 'application/json'}
 #     response = requests.get(url, headers=headers)
 #     pprint(response.json())
 #
@@ -23,23 +23,26 @@ print(clever_hero, best_intelligence)
 #     test_request()
 
 # TASK 2
+import requests
 
 class YaUploader:
     def __init__(self, file_path: str):
         self.file_path = file_path
 
     def upload(self):
-        url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
-        files = {'file': open('test_upload.txt', 'rb')}
-        headers = {'Authorization': 'OAuth AQAAAAAFb6IVAADLW6pIWXkR-k0gsGDbokios1I',
-                   'Content-Type': 'application/json'}
-        response = requests.put(url=url, headers=headers, files=files)
-        print(response.status_code)
-        if response.status_code == 201:
-            print("Success")
+        PREPARE_UPLOAD_URL = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        params = {'path':'test_upload.txt', 'overwrite': 'true'}
+        TOKEN = 'OAuth ****************************************'
+        headers = {'Accept': 'application/json', 'Authorization': TOKEN}
+
+        response = requests.get(PREPARE_UPLOAD_URL, params=params, headers=headers)
+        put_url = response.json().get('href')
+
+        files = {'file': open(self.file_path, 'rb')}
+        response = requests.put(put_url, files=files, headers=headers)
+        if response.status_code == 201: print('File successfully uploaded.')
 
 if __name__ == '__main__':
     uploader = YaUploader('test_upload.txt')
     result = uploader.upload()
-    """ссылка на файл в компьютере"""
-# 'C:\Users\79122\PycharmProjects\pythonProject\requests_test\test_upload.txt'
+
--- requests_test/temp.py
+++ requests_test/temp.py
@@ -1,8 +1,17 @@
-def test_request():
-    url = "https://cloud-api.yandex.net/v1/disk/resources/files"
-    headers = {'Authorization': 'OAuth AQAAAAAFb6IVAADLW6pIWXkR-k0gsGDbokios1I', 'Content-Type': 'application/json'}
-    response = requests.get(url, headers=headers)
-    pprint(response.json())
 
-if __name__ == '__main__':
-    test_request()
+# PREPARE_UPLOAD_URL = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+# params = {'path': '/9.txt', 'overwrite': 'true'}
+# TOKEN = 'OAuth AQAAAAAFb6IVAADLW6pIWXk'
+# headers = {'Accept': 'application/json', 'Authorization': TOKEN}
+#
+# file_path = 'recipes.txt'
+#
+# response = requests.get(PREPARE_UPLOAD_URL, params=params, headers=headers)
+# print(response.text)
+#
+# put_url = response.json().get('href')
+# print(put_url)
+#
+# files = {'file': open(file_path, 'rb')}
+# response = requests.put(put_url, files=files, headers=headers)
+# print(response)
